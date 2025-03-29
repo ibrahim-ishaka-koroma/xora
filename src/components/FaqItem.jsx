eb54d6a7481c7e52255a82a72bab437e30ef572e
ibrahim ishaka Kororma <ibrahimishakakoroma535@gmail.com>
2025-03-29 09:37:59 +0000

    removed

--- src/components/FaqItem.jsx
+++ src/components/FaqItem.jsx
@@ -1,7 +1,7 @@
 import clsx from "clsx";
 import { useState } from "react";
-import { SlideDown } from "react-slidedown";
-import "react-slidedown/lib/slidedown.css";
+// import { SlideDown } from "react-slidedown";
+// import "react-slidedown/lib/slidedown.css";
 
 const FaqItem = ({ item, index }) => {
   const [activeId, setActiveId] = useState(null);
@@ -41,11 +41,11 @@ const FaqItem = ({ item, index }) => {
         </div>
       </div>
 
-      <SlideDown>
+      {/* <SlideDown> */}
         {activeId === item.id && (
           <div className="body-3 px-7 py-3.5">{item.answer}</div>
         )}
-      </SlideDown>
+      {/* </SlideDown> */}
 
       <div
         className={clsx(
